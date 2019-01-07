d0af6dff98717080115792c3d60dc13d30d83df6
Kartik Chauhan <kckartik17@gmail.com>
2019-01-08 01:48:06 +0530

    Day 02: Passport JWT Authentication Strategy
    Signed-off-by: Kartik Chauhan <kckartik17@gmail.com>

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
+
+//Passport Middleware
+app.use(passport.initialize());
+
+//Passport config
+require("./config/passport")(passport);
 
 //Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,9 +34,6 @@ mongoose
   .catch(err => console.log(err));
 
 // Routes
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
 
 app.use("/api/users", users);
 app.use("/api/profile", profile);
