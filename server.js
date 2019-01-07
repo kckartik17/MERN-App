5f1b57c39dee590af339a1d5be4902cbc909d320
Kartik Chauhan <kckartik17@gmail.com>
2019-01-07 22:06:29 +0530

    Day 01: Initial Express server with route files
    Signed-off-by: Kartik Chauhan <kckartik17@gmail.com>

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
+const app = express();
+
+//Import keys
+const keys = require("./config/keys");
+
+//Database config
+mongoose
+  .connect(
+    keys.mongoURI,
+    {
+      useNewUrlParser: true
+    }
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
