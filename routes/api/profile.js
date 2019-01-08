ffb9264fde911bad2ded676348b9eec705b6d489
Kartik Chauhan <kckartik17@gmail.com>
2019-01-08 23:16:22 +0530

    Day 02: Current User profile route
    Signed-off-by: Kartik Chauhan <kckartik17@gmail.com>

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,38 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+//Load Models
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+//@route GET api/profile/test
+//@desc Tests profile route
+//@access Public
 
 router.get("/test", (req, res) => res.json({ msg: "Profile works" }));
+
+//@route GET api/profile
+//@desc Get current user profile
+//@access Private
+
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then(foundUser => {
+        if (!foundUser) {
+          errors.noprofile = "There is no profile for this user";
+          return res.status(404).json(errors);
+        }
+        res.json(foundUser);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 
 module.exports = router;
