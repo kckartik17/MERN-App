507517b55245d4126caee597554944a8f0c649e1
Kartik Chauhan <kckartik17@gmail.com>
2019-02-09 03:10:39 +0530

    Form Field Components
    Signed-off-by: Kartik Chauhan <kckartik17@gmail.com>

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import TextFieldGroup from "../common/TextFieldGroup";
+import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
+import SelectListGroup from "../common/SelectListGroup";
+import InputGroup from "../common/InputGroup";
 
 class CreateProfile extends Component {
   constructor(props) {
