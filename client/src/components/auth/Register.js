79cc7b6a5519ab81659eb6d1449e55b1b61d0ba2
Kartik Chauhan <kckartik17@gmail.com>
2019-02-08 22:16:13 +0530

    Registration - The Error Reducer
    Signed-off-by: Kartik Chauhan <kckartik17@gmail.com>

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
 import classnames from "classnames";
 import { connect } from "react-redux";
 import { registerUser } from "../../actions/authActions";
@@ -39,7 +40,7 @@ class Register extends Component {
       password2: this.state.password2
     };
 
-    this.props.registerUser(newuser);
+    this.props.registerUser(newuser, this.props.history);
   }
 
   render() {
@@ -140,4 +141,4 @@ const mapStateToProps = state => ({
 export default connect(
   mapStateToProps,
   { registerUser }
-)(Register);
+)(withRouter(Register));
