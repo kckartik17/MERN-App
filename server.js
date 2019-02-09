caffb2b153ebc64df08b97e8f6aaa73b4db03b14
Kartik Chauhan <kckartik17@gmail.com>
2019-02-10 00:23:11 +0530

    preparing for deployment
    Signed-off-by: Kartik Chauhan <kckartik17@gmail.com>

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -24,12 +25,9 @@ const keys = require("./config/keys");
 
 //Database config
 mongoose
-  .connect(
-    keys.mongoURI,
-    {
-      useNewUrlParser: true
-    }
-  )
+  .connect(keys.mongoURI, {
+    useNewUrlParser: true
+  })
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
@@ -38,6 +36,16 @@ mongoose
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+//Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
