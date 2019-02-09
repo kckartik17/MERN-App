3dc1ce6e506ad621c09842548746048bea2efaca
Kartik Chauhan <kckartik17@gmail.com>
2019-02-09 15:34:19 +0530

    Add Experience Form
    Signed-off-by: Kartik Chauhan <kckartik17@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ import { clearCurrentProfile } from "./actions/profileActions";
 
 import "./App.css";
 import EditProfile from "./components/edit-profile/EditProfile";
+import AddExperience from "./components/add-credentials/AddExperience";
 
 //Check for token
 if (localStorage.jwtToken) {
@@ -68,6 +69,13 @@ class App extends Component {
                   exact
                   path="/edit-profile"
                   component={EditProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-experience"
+                  component={AddExperience}
                 />
               </Switch>
             </div>
