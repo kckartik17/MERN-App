5b0afaeedaf614769f12f3edf0361d9dd408fd2a
Kartik Chauhan <kckartik17@gmail.com>
2019-02-10 11:54:18 +0530

    Bug fixing
    Signed-off-by: Kartik Chauhan <kckartik17@gmail.com>

--- client/src/store.js
+++ client/src/store.js
@@ -6,11 +6,12 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 const store = createStore(
   rootReducer,
   initialState,
-  composeEnhancers(applyMiddleware(...middleware))
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 export default store;
